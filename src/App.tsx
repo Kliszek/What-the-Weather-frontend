93864f950928c9e395e61c468bef4837ae574119
Jakub Kliszko <kuba.kliszko@gmail.com>
2022-06-27 00:04:34 +0200

    Added WeatherBlock

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { WeatherBlock } from "./WeatherBlock";
 import { WeatherNav } from "./WeatherNav";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App flex flex-col h-screen">
       <WeatherNav />
-      <header className="App-header"></header>
+      <header className="bg-slate-200 h-full flex justify-center">
+        <div className="w-2/3 bg-white flex flex-col justify-center">
+          <div className="w-2/3 mx-auto">
+            <WeatherBlock />
+          </div>
+        </div>
+      </header>
     </div>
   );
 }
