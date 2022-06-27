d7434d6b9d3d8a1512a1a9ddda70510458b309e1
Jakub Kliszko <kuba.kliszko@gmail.com>
2022-06-27 17:39:52 +0200

    Added BACKEND_URL environmental variable

--- src/services/weather.service.ts
+++ src/services/weather.service.ts
@@ -1,10 +1,12 @@
 import axios from "axios";
 
-const BACKEND_URL = "http://localhost:3000/v1/api/weather";
+const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:3000";
 
 export default class WeatherService {
   static async getWeather(cityName: string | undefined, options: object = {}) {
-    const requestURL = `${BACKEND_URL}/${cityName ? cityName : ""}`;
+    const requestURL = `${BACKEND_URL}//v1/api/weather/${
+      cityName ? cityName : ""
+    }`;
     return axios.get(requestURL, options);
   }
 }
