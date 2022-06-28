324f3e1d936cb2ea08e2089881e873e9a4102c41
Jakub Kliszko <kuba.kliszko@gmail.com>
2022-06-28 04:41:23 +0200

    Added CORS

--- src/services/weather.service.ts
+++ src/services/weather.service.ts
@@ -5,9 +5,21 @@ const BACKEND_URL =
 
 export default class WeatherService {
   static async getWeather(cityName: string | undefined, options: object = {}) {
+    Object.assign(options, this._getCommonOptions());
     const requestURL = `${BACKEND_URL}/v1/api/weather/${
       cityName ? cityName : ""
     }`;
     return axios.get(requestURL, options);
   }
+
+  static _getCommonOptions = () => ({
+    data: {},
+    headers: {
+      "Access-Control-Allow-Origin": "https://what-the-weather-1.herokuapp.com",
+      "Access-Control-Allow-Headers":
+        "Accept, Content-Type, Origin, X-Requested-With, x-api-key",
+      "Access-Control-Allow-Methods": "*",
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  });
 }
